40212c374948fd4a201ed8f223976d6223cd06bd
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2019-07-14 15:38:08 -0400

    created animated header views

--- src/Main.tsx
+++ src/Main.tsx
@@ -24,30 +24,20 @@ class Main extends Component {
 
     // remember Animated destructured
     this.scrollY = new Value(0);
-  }
 
-  componentDidMount() {
-    // setInterval(() => {
-    //   for (let i = 0; i < 1000; i++) {
-    //     console.log(i);
-    //   }
-    // }, 200);
-  }
-
-  render() {
-    const headerHeight = this.scrollY.interpolate({
+    this.headerHeight = this.scrollY.interpolate({
       inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
       outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
       extrapolate: Extrapolate.CLAMP
     });
 
-    const profileImageHeight = this.scrollY.interpolate({
+    this.profileImageHeight = this.scrollY.interpolate({
       inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
       outputRange: [PROFILE_IMAGE_MAX_HEIGHT, PROFILE_IMAGE_MIN_HEIGHT],
       extrapolate: Extrapolate.CLAMP
     });
 
-    const profileImageMarginTop = this.scrollY.interpolate({
+    this.profileImageMarginTop = this.scrollY.interpolate({
       inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
       outputRange: [
         HEADER_MAX_HEIGHT - PROFILE_IMAGE_MAX_HEIGHT / 2,
@@ -56,13 +46,13 @@ class Main extends Component {
       extrapolate: Extrapolate.CLAMP
     });
 
-    const headerZindex = this.scrollY.interpolate({
+    this.headerZindex = this.scrollY.interpolate({
       inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT, 120],
       outputRange: [0, 0, 1000],
       extrapolate: Extrapolate.CLAMP
     });
 
-    const headerTitleBottom = this.scrollY.interpolate({
+    this.headerTitleBottom = this.scrollY.interpolate({
       inputRange: [
         0,
         HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT,
@@ -72,10 +62,78 @@ class Main extends Component {
       outputRange: [-20, -20, -20, 0],
       extrapolate: Extrapolate.CLAMP
     });
+  }
 
+  componentDidMount() {
+    setInterval(() => {
+      for (let i = 0; i < 1000; i++) {
+        console.log(i);
+      }
+    }, 200);
+  }
+
+  render() {
     return (
       <View style={styles.root}>
-        <Text>Main</Text>
+        <Animated.View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: "lightskyblue",
+            height: this.headerHeight,
+            zIndex: this.headerZindex,
+            elevation: this.headerZindex,
+            alignItems: "center"
+          }}
+        >
+          <Animated.View
+            style={{
+              position: "absolute",
+              bottom: this.headerTitleBottom
+            }}
+          >
+            <Text style={styles.headerText}>Caption Caption</Text>
+          </Animated.View>
+        </Animated.View>
+
+        <Animated.ScrollView
+          style={{ flex: 1 }}
+          scrollEventThrottle={16}
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: { y: this.scrollY }
+              }
+            }
+          ])}
+        >
+          <Animated.View
+            style={{
+              height: this.profileImageHeight,
+              width: this.profileImageHeight,
+              borderRadius: PROFILE_IMAGE_MAX_HEIGHT / 2,
+              borderColor: "white",
+              borderWidth: 3,
+              overflow: "hidden",
+              marginTop: this.profileImageMarginTop,
+              marginLeft: 10
+            }}
+          >
+            <Image
+              source={{
+                uri: "https://avatars1.githubusercontent.com/u/6421596?s=460&v=4"
+              }}
+              style={styles.profileImage}
+            />
+          </Animated.View>
+          <View>
+            <Text style={styles.imageText}>Caption Caption</Text>
+          </View>
+
+          <View style={styles.divide} />
+        </Animated.ScrollView>
       </View>
     );
   }
@@ -83,9 +141,25 @@ class Main extends Component {
 
 const styles = StyleSheet.create({
   root: {
+    flex: 1
+  },
+  headerText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "bold"
+  },
+  profileImage: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    width: null,
+    height: null
+  },
+  imageText: {
+    fontWeight: "bold",
+    fontSize: 26,
+    paddingLeft: 10
+  },
+  divide: {
+    height: 1000
   }
 });
 
